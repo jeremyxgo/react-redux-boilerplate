93f020e375a9602ea31eb0851b1748f6d498a2fa
Jeremy Go <jeremy.x.go@gmail.com>
2018-01-12 01:17:58 +0800

    Persist and rehydrate store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
 import { Router, Route } from 'react-router-dom';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { createBrowserHistory } from 'history';
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import storage from 'redux-persist/lib/storage'
 import createSagaMiddleware from 'redux-saga';
-import { Provider } from 'react-redux';
-import { AppContainer } from 'react-hot-loader';
 import App from './containers/App';
-import reducer from './reducers';
+import reducers from './reducers';
 import saga from './sagas';
 
+const persistConfig = { key: 'root', version: 1, storage };
 const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
+const reducer = persistReducer(persistConfig, reducers);
 const store = createStore(reducer, compose(applyMiddleware(sagaMiddleware)));
+const persistor = persistStore(store);
 
 let sagaTask = sagaMiddleware.run(saga);
 
@@ -20,9 +26,11 @@ const render = Component => {
   ReactDom.render(
     <AppContainer warnings={false}>
       <Provider store={store}>
-        <Router history={history}>
-          <Route path="/" component={Component} />
-        </Router>
+        <PersistGate persistor={persistor}>
+          <Router history={history}>
+            <Route path="/" component={Component}/>
+          </Router>
+        </PersistGate>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
@@ -37,7 +45,9 @@ if (module.hot) {
   });
 
   module.hot.accept('./reducers', () => {
-    store.replaceReducer(reducer);
+    store.replaceReducer(
+      persistReducer(persistConfig, reducers)
+    );
   });
 
   module.hot.accept('./sagas', () => {
